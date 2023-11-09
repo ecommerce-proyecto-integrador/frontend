e8fab4a2345b85c960919fb2206a03e116a9cb5f
Whiserber <whiserber@gmail.com>
2023-11-08 22:18:55 -0300

    Fix merge conflict

--- src/app/apolloClient.tsx
+++ src/app/apolloClient.tsx
@@ -1,28 +1,44 @@
-'use client'; 
-import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+"use client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+  from,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { onError } from "@apollo/client/link/error";
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.forEach(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+      )
+    );
+  if (networkError) console.log(`[Network error]: ${networkError}`);
+});
+
 const httpLink = createHttpLink({
-  uri: 'http://192.168.1.82:4000/graphql', // <-- Add the URL of the GraphQL server here
+  uri: "http://192.168.1.82:4000/graphql", // <-- Add the URL of the GraphQL server here 192.168.1.82:4000
 });
 
 const authLink = setContext(async (_, { headers }) => {
   // get the authentication token from local storage if it exists
   //Here logic to get token from local storage
   const token = "sadasdasdas"; //change this
-  
+
   // return the headers to the context so httpLink can read them
   return {
     headers: {
       ...headers,
       authorization: token,
-    }
-  }
+    },
+  };
 });
 
 const client = new ApolloClient({
-    link: authLink.concat(httpLink),
-    cache: new InMemoryCache(),
-  });
+  link: from([errorLink, httpLink]),
+  cache: new InMemoryCache(),
+});
 
-  export default client;
+export default client;
--- src/app/components/product/Products.tsx
+++ src/app/components/product/Products.tsx
@@ -1,6 +1,20 @@
-import React, { useState, useEffect } from 'react';
+'use client';
+import React, { useState, useEffect} from 'react';
 import ProductCard from './ProductCard';
-import { products } from '../../../../utils/products';
+//import { products } from '../../../../utils/products';
+import { useQuery, gql } from '@apollo/client';
+type Product = {
+  ID: string;
+  name: string;
+  description: string;
+  price: number;
+  brand: string;
+  inStock: boolean;
+  sizeAvailable: string[];
+  image: string;
+  reviews: string[];
+  Category: string;
+};
 
 const Products: React.FC = () => {
   const [categoryFilter, setCategoryFilter] = useState('');
@@ -8,16 +22,38 @@ const Products: React.FC = () => {
   const [priceFilter, setPriceFilter] = useState(0);
   const [categories, setCategories] = useState<string[]>([]);
   const [brands, setBrands] = useState<string[]>([]);
+  const [datafetch, setDatafetch] = useState("");
+  const [products, setProducts] = useState<Product[]>([]);
+
+  const getProducts = gql`
+    query getProducts {
+      getProducts
+    }
+  `;
+
+  const { loading, error, data, refetch: refetchProducts } = useQuery(getProducts);
 
   useEffect(() => {
     // Extracting unique categories and brands from products
-    const uniqueCategories = Array.from(new Set(products.map(product => product.category)));
-    const uniqueBrands = Array.from(new Set(products.map(product => product.brand)));
+    if (!loading && !error && data) {
+      console.log("Data", data.getProducts)
+      setDatafetch(data.getProducts);
+      const json = JSON.parse(data.getProducts)
+      setProducts(json);
 
-    setCategories(['', ...uniqueCategories]); // Include an option for all categories
-    setBrands(['', ...uniqueBrands]); // Include an option for all brands
-  }, []);
+      const uniqueCategories = Array.from(new Set(json.map(product  => product.Category)));
+      const uniqueBrands = Array.from(new Set(json.map(product  => product.brand)));
+      
+      console.log("UniqueCategories", uniqueCategories)
+      console.log("UniqueBrands", uniqueBrands)
+      setCategories(['', ...uniqueCategories]); // Include an option for all categories
+      setBrands(['', ...uniqueBrands]); // Include an option for all brands
+    }
+    
+  }, [loading, error, data]);
 
+  //console.log("Datajson", products)
+  //console.log("Productos", products);
   const handleCategoryChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setCategoryFilter(e.target.value);
   };
@@ -32,22 +68,21 @@ const Products: React.FC = () => {
 
   const filteredProducts = products.filter(product => {
     return (
-      (categoryFilter === '' || product.category === categoryFilter) &&
+      (categoryFilter === '' || product.Category === categoryFilter) &&
       (brandFilter === '' || product.brand === brandFilter) &&
       (priceFilter === 0 || product.price <= priceFilter)
     );
   });
-
   return (
     <div className="max-w-screen-xl mx-auto my-8 px-4">
-      <div className="bg-white margin p-5 rounded-xl mb-10">
+      <div className="bg-white margin p-5 rounded-xl mb-10 shadow-lg">
         <div className='flex justify-between items-center'>
           {/* Category Filter */}
           <select
             value={categoryFilter}
             onChange={handleCategoryChange}
             className="block py-2.5 px-0 w-[200px] text-sm font-semibold text-gray-500
-            bg-transparent border-0 border-b-2 border-gray-200 appearance-none
+            bg-transparent border-0 border-b-2 border-gray-200 appearance-none cursor-pointer
             dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer"
           >
             {categories.map(category => (
@@ -62,7 +97,7 @@ const Products: React.FC = () => {
             value={brandFilter}
             onChange={handleBrandChange}
             className="block py-2.5 px-0 w-[200px] text-sm text-gray-500 font-semibold
-            bg-transparent border-0 border-b-2 border-gray-200 appearance-none
+            bg-transparent border-0 border-b-2 border-gray-200 appearance-none cursor-pointer
             dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer"
           >
             {brands.map(brand => (
@@ -79,7 +114,7 @@ const Products: React.FC = () => {
             onChange={handlePriceChange}
             placeholder="Max Price"
             className="block py-2.5 px-0 w-[200px] text-sm text-gray-500 font-semibold
-            bg-transparent border-0 border-b-2 border-gray-200 appearance-none
+            bg-transparent border-0 border-b-2 border-gray-200 appearance-none cursor-pointer
             dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer"
           />
         </div>
@@ -87,7 +122,7 @@ const Products: React.FC = () => {
 
       <div className="grid grid-cols-1 md:grid-cols-3 gap-12">
         {filteredProducts.map(product => (
-          <ProductCard key={product.id} data={product} />
+          <ProductCard key={product.ID} data={product} />
         ))}
       </div>
     </div>
