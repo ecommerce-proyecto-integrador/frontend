1f4ee7162c5748e142ac1eed472d96c7a0ade7cf
Diego González <diego.gonzalez07@alumnos.ucn.cl>
2023-12-21 03:54:12 -0300

    Fix graphql

--- src/app/pages/login/pass-recovery-validation/page.tsx
+++ src/app/pages/login/pass-recovery-validation/page.tsx
@@ -5,7 +5,7 @@ import GraphQlProvider from "../../../GraphQLProvider";
 
 const PasswordRecoveryValidationPage = () => {
   return (
-    <GraphQlProvider children={undefined}>
+    <GraphQlProvider>
       <Container>
         <FormWrap>
           <ValidationForm />
